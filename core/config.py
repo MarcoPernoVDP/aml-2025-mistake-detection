fa043d12d3904a22d78ad29c86476b2a8070db8f
Dylan <s337915@studenti.polito.it>
2025-12-11 16:43:36 +0100

    modifiche al config

--- core/config.py
+++ core/config.py
@@ -17,7 +17,7 @@ class Config(object):
         # Use this for 1 sec video features
         self.segment_features_directory = "data/"
 
-        self.ckpt_directory = "/data/rohith/captain_cook/checkpoints/"
+        self.ckpt_directory = "/checkpoints/"
         self.split = "step"
         self.batch_size = 1
         self.test_batch_size = 1
@@ -62,9 +62,9 @@ class Config(object):
         parser.add_argument("--seed", type=int, default=42, help="random seed (default: 1000)")
 
         parser.add_argument("--backbone", type=str, default=const.OMNIVORE, help="backbone model")
-        parser.add_argument("--ckpt_directory", type=str, default="/data/rohith/captain_cook/checkpoints", help="checkpoint directory")
+        parser.add_argument("--ckpt_directory", type=str, default="./checkpoints", help="checkpoint directory")
         parser.add_argument("--split", type=str, default=const.RECORDINGS_SPLIT, help="split")
-        parser.add_argument("--variant", type=str, default=const.TRANSFORMER_VARIANT, help="variant")
+        parser.add_argument("--variant", type=str, default=const.RNN_VARIANT, help="variant")
         parser.add_argument("--model_name", type=str, default=None, help="model name")
         parser.add_argument("--task_name", type=str, default=const.ERROR_RECOGNITION, help="task name")
         parser.add_argument("--error_category", type=str, help="error category")
